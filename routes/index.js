98682f29c19f060d9a16e76640dee46a424e2135
Ubuntu <ubuntu@ip-172-31-40-235.us-west-2.compute.internal>
2016-07-21 14:08:19 +0000

    init

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Oops we seem to be down right now' });
+});
+
+module.exports = router;
